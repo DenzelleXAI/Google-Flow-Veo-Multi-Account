f57da1055e157a3b20c5c414a2239259353a29f7
Denzelle John <denzellejohnpascuadoromal@gmail.com>
2026-09-09 06:53:27 +0800

    chore: set Inngest route duration for Vercel

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -2,6 +2,8 @@ import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest";
 import { monitorVeoGeneration, submitVeoGenerationJob } from "@/lib/generation-worker";
 
+export const maxDuration = 300;
+
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [submitVeoGenerationJob, monitorVeoGeneration],
